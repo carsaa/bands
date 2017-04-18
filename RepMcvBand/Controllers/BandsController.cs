98365b4a786c5a0acd224c082fba28cf665ba6cd
Carolina Saavedra <carro_saavedra@hotmail.com>
2017-04-18 21:01:25 +0200

    Add project files.

---
+++ RepMcvBand/Controllers/BandsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using static RepMcvBand.Models.DataManager;
+
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RepMcvBand.Controllers
+{
+    public class BandsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Bands = GetAllBands();
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            ViewBag.Band = GetBandById(id);
+            return View();
+        }
+    }
+}
---
+++ RepMcvBand/Models/DataManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepMcvBand.Models
+{
+
+    public static class DataManager
+    {
+        static List<Band> bands = new List<Band>
+        {
+            new Band {Id = 1, Name = "Coldplay", Description = "British pop band with lead singer Chris Martin"},
+            new Band {Id = 2, Name = "Frank Ocean", Description = "American rapper known for his album Orange"},
+            new Band {Id = 3, Name = "Joshua Radin", Description = "Singer-songwriter from America"}
+        };
+
+        public static Band GetBandById(int id)
+        {
+            return bands[id];     
+        }
+
+        public static Band[] GetAllBands()
+        {
+            return bands.ToArray();  
+        }
+
+    }
+
+}
